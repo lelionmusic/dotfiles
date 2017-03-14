4df51459d407a05d30de8d49fde418ea8789bc9d
Geir Olav Alsvik <goagoa95@gmail.com>
2017-03-14 22:11:27 +0100

    efiwof:

---
+++ scripts/rofi.sh
@@ -0,0 +1,1 @@
+rofi -show run -width 100 -lines 6 -font "San Francisco Display Thin 16" -padding 450 -color-normal "#00ffffff,white,#00ffffff,#00ffffff,#4c88ad" -color-window "#B81b2023,#00ffffff,#00ffffff" -bw 0
