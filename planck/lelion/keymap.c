ff8c09a494d115c196b0cc70cf719b0b51a4d104
geiroal <geiroal@student.matnat.uio.no>
2018-10-04 14:46:33 +0200

    changed keymap

--- planck/lelion/keymap.c
+++ planck/lelion/keymap.c
@@ -58,12 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *     when held   |------+------+------+------+------+------+------+------+------+------+------+------|
  *                 | Caps |      |  Os  | Alt  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  *                 `-----------------------------------------------------------------------------------'
+ * Space = lower when held
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_ESC, KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, LT(_NAV, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
-  {KC_CAPS, KC_RSFT,        KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_CAPS, _______,        KC_LALT, KC_LGUI, KC_LALT,   KC_SPC, KC_SPC,  RAISE,    LOWER, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -90,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |      |      |   _  |   -  |      |      |   +  |   =  |      |      | Del  |
+ * |   `  |      |      |      |      |      |      |   -  |   =  |   [  |   ]  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -101,8 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 
+
 [_RAISE] = {
-    {KC_GRV, _______, _______, S(KC_MINS), KC_MINS, _______, _______, S(KC_EQL), KC_EQL, _______, _______, KC_DEL},
+    {KC_GRV, _______, _______, KC_LPRN, KC_RPRN, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_DEL},
     {_______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS},
     {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______,  _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU,  KC_MNXT}},
