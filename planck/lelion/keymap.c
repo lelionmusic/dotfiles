d96807db90f70ffed957cafb9e2f8ea075b4a362
geiroal <geiroal@student.matnat.uio.no>
2018-10-04 15:54:24 +0200

    sync

--- planck/lelion/keymap.c
+++ planck/lelion/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_ESC, KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, LT(_NAV, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
-  {KC_CAPS, _______,        _______, KC_LGUI, KC_LALT,   KC_SPC, KC_SPC,  RAISE,    LOWER, KC_DOWN, KC_UP,   KC_RGHT}},
+  {KC_CAPS, _______,        _______, KC_LALT, KC_LGUI, KC_SPC, KC_SPC,  RAISE,    LOWER, KC_DOWN, KC_UP,   KC_RGHT}},
 
 
 /* Lower
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {AG_NORM, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, RESET,   MU_MOD,  AU_ON,   AU_OFF,  _______, _______, _______, _______, _______, _______, _______},
   {MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  _______, _______, _______, _______, _______, _______, BL_BRTG, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}},
